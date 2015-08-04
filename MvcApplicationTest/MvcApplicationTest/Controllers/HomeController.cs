b596647dda503ac70be0e8feaf038253a451ff3b
Lian Wang <kohlemo@gmail.com>
2015-08-04 13:51:47 +1200

    Recreate the project

--- MvcApplicationTest/MvcApplicationTest/Controllers/HomeController.cs
+++ MvcApplicationTest/MvcApplicationTest/Controllers/HomeController.cs
@@ -6,27 +6,25 @@ using System.Web.Mvc;
 
 namespace MvcApplicationTest.Controllers
 {
-    public class HomeController : Controller
-    {
-        public ActionResult Index()
-        {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
 
-            return View();
-        }
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your app description page.";
+			return View();
+		}
 
-            return View();
-        }
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-    }
+			return View();
+		}
+	}
 }
--- MvcApplicationTest/MvcApplicationTest/Properties/AssemblyInfo.cs
+++ MvcApplicationTest/MvcApplicationTest/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MvcApplicationTest")]
@@ -14,22 +14,22 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("433c33eb-5902-4d8a-9fd0-59ec62dc69ca")]
+[assembly: Guid("95ec8295-baef-4f30-bb10-9e2c4f045556")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
